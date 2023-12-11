8333c52707a96f82d4f2b63751677c597a3ad8d4
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-12 00:24:06 +0200

    bugfix: minionsId parameter didn't need to be Number data type

--- server/api/minions.js
+++ server/api/minions.js
@@ -5,7 +5,6 @@ const modelType = "minions";
 module.exports = minionsRouter;
 
 minionsRouter.param("minionId", (req, res, next, id) => {
-    id = Number(id);
     const minion = db.getFromDatabaseById(modelType, id);
     if (minion) {
         req.minion = minion;
