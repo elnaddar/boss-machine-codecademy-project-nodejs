f0b53b07c7a001f99acf30e742caf13795fd1c53
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-12 01:51:03 +0200

    POST /api/minions/:minionId/work to create a new work object and save it to the database.

--- server/api/work.js
+++ server/api/work.js
@@ -8,3 +8,12 @@ workRouter.get("/", (req, res, next) => {
     const work = db.getAllFromDatabase(modelType).filter((work)=>{work.minionId == req.minionId});
     res.send(work);
 });
+
+workRouter.post("/", (req, res, next) => {
+    const instance = db.addToDatabase(modelType, req.body);
+    if (instance) {
+        res.status(201).send(instance);
+    } else {
+        res.sendStatus(400);
+    }
+});
