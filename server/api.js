0ae784d9db0c9142ed33980110a160a3224af114
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-12 01:00:06 +0200

    init /api/meetings/

--- server/api.js
+++ server/api.js
@@ -3,6 +3,7 @@ const apiRouter = express.Router();
 
 apiRouter.use("/minions", require('./api/minions'));
 apiRouter.use("/ideas", require('./api/ideas'));
+apiRouter.use("/meetings", require('./api/meetings'));
 
 
 module.exports = apiRouter;
