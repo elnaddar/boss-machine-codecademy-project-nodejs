31b5d6a3961978f54d84cf8b9eca0e373b70afac
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-12 00:57:48 +0200

    created idea endpoint just copied same code for minions.

--- server/api.js
+++ server/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-apiRouter.use("/minions", require('./api/minions'))
+apiRouter.use("/minions", require('./api/minions'));
+apiRouter.use("/ideas", require('./api/ideas'));
 
 
 module.exports = apiRouter;
---
+++ server/api/ideas.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const ideasRouter = express.Router();
+const db = require("../db");
+const modelType = "ideas";
+module.exports = ideasRouter;
+
+ideasRouter.param("ideaId", (req, res, next, id) => {
+    const idea = db.getFromDatabaseById(modelType, id);
+    if (idea) {
+        req.idea = idea;
+        req.ideaId = id;
+        next();
+    }
+    res.sendStatus(404);
+});
+
+ideasRouter.get("/", (req, res, next) => {
+    res.send(db.getAllFromDatabase(modelType));
+});
+
+ideasRouter.post("/", (req, res, next) => {
+    const instance = db.addToDatabase(modelType, req.body);
+    if (instance) {
+        res.status(201).send(instance);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+ideasRouter.get("/:ideaId", (req, res, next) => {
+    res.send(req.idea);
+});
+
+ideasRouter.put("/:ideaId", (req, res, next) => {
+    const instance = db.updateInstanceInDatabase(modelType, req.body);
+    if (instance) {
+        res.send(instance);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+ideasRouter.delete("/:ideaId", (req, res, next) => {
+    const isDeleted = db.deleteFromDatabasebyId(modelType, req.ideaId);
+    if (isDeleted) {
+        res.sendStatus(204);
+    } else {
+        res.sendStatus(400);
+    }
+})
