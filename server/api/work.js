6bde3f3f5d055bf2034f020234614b6c9f710a36
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-12 01:49:59 +0200

    finish GET /api/minions/:minionId/work to get an array of all work for the specified minion.

--- server/api/work.js
+++ server/api/work.js
@@ -3,3 +3,8 @@ const workRouter = express.Router({mergeParams: true});
 const db = require("../db");
 const modelType = "work";
 module.exports = workRouter;
+
+workRouter.get("/", (req, res, next) => {
+    const work = db.getAllFromDatabase(modelType).filter((work)=>{work.minionId == req.minionId});
+    res.send(work);
+});
