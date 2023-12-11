587f4cdb3d9fac4ed2f541c27e462afc2e92427d
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-11 23:53:56 +0200

    created middleware for minionId param for api/minions

--- server/api/minions.js
+++ server/api/minions.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const minionsRouter = express.Router();
 const db = require("../db");
-
+const modelType = "minions";
 module.exports = minionsRouter;
+
+minionsRouter.param("minionId", (req, res, next, id)=>{
+    id = Number(id);
+    const minion = db.getFromDatabaseById(modelType, id);
+    if(minion){
+        req.minion = minion;
+        next();
+    }
+    res.sendStatus(404);
+});
